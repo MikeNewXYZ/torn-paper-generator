4856ff3aad93eccb7d49c9b86bd419becaf95636
NotBaldrick <Git@NotBaldrick.com>
2022-06-15 16:50:34 +0100

    add wigglyeffect component

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,4 @@
 export {default as PaperPlatform} from "./PaperPlatform";
 export {default as ResultsPaper} from "./ResultsPaper";
+export {default as CopyStyles} from "./CopyStyles";
+export {default as WigglyEffect} from "./WigglyEffect";
