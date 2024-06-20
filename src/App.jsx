ad23e1eb2986d756c601de60e9d8927580f6a8c7
Mike Newberry <hello@mikenew.xyz>
2024-06-20 23:00:22 +0100

    add options component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,58 @@
 import "./App.css";
+import { useCallback, useState } from "react";
 import Paper from "./components/paper/paper";
+import Options from "./components/options/options";
 
 export default function App() {
+	const [optionValues, setOptionValues] = useState({
+		topSide: true,
+		bottomSide: true,
+		leftSide: true,
+		rightSide: true,
+		amountOfPoints: 50,
+		roughness: 50,
+	});
+
+	const handleOptions = useCallback((e) => {
+		const { type, checked, value, name } = e.target;
+
+		setOptionValues((prev) => ({
+			...prev,
+			[name]: type === "checkbox" ? checked : parseInt(value),
+		}));
+	}, []);
+
+	const handleCopyStyles = useCallback(
+		(e) => {
+			e.preventDefault();
+		},
+		[optionValues],
+	);
+
+	const handleRefresh = useCallback(
+		(e) => {
+			e.preventDefault();
+		},
+		[optionValues],
+	);
+
 	return (
-		<main className="w-full h-full flex justify-center items-center pb-10 pt-8 px-2">
-			<Paper />
+		<main className="w-full h-full flex justify-center items-center pt-2 pb-2 md:landscape:pb-4 px-2">
+			<Paper>
+				<div className="w-full h-full px-4 pb-4 pt-3 flex flex-col text-xs sm:text-base overflow-y-auto">
+					<div className="flex justify-between mb-8">
+						<h1>Torn Paper Generator</h1>
+						<p className="text-right">By MikeNewXYZ</p>
+					</div>
+
+					<Options
+						handleOptions={handleOptions}
+						options={optionValues}
+						handleCopyStyles={handleCopyStyles}
+						handleRefresh={handleRefresh}
+					/>
+				</div>
+			</Paper>
 		</main>
 	);
 }
---
+++ src/components/options/options.jsx
@@ -0,0 +1,106 @@
+function Checkbox({ title = "", checked, onChange, ...props }) {
+	return (
+		<label htmlFor={title} className="flex items-center gap-4">
+			<input
+				{...props}
+				type="checkbox"
+				checked={checked}
+				onChange={onChange}
+				className="checkbox checkbox-xs sm:checkbox-md"
+				id={title}
+				aria-checked={checked}
+				aria-label={title}
+			/>
+			<span>{title}</span>
+		</label>
+	);
+}
+
+function Range({ title = "", value, onChange, ...props }) {
+	return (
+		<label htmlFor={title} className="flex flex-col gap-2">
+			<span>
+				{title} ({value})
+			</span>
+			<input
+				{...props}
+				min={0}
+				max={100}
+				value={value}
+				onChange={onChange}
+				type="range"
+				className="range range-xs sm:range-md"
+				id={title}
+				aria-valuemin={0}
+				aria-valuemax={100}
+				aria-valuenow={value}
+				aria-label={title}
+			/>
+		</label>
+	);
+}
+
+export default function Options({ handleOptions, options, handleCopyStyles, handleRefresh }) {
+	return (
+		<form className="w-full flex flex-col gap-4 grow justify-center">
+			<div className="flex flex-col gap-4 mb-2">
+				<span>Affected Sides</span>
+
+				<Checkbox
+					title="Top Side"
+					name="topSide"
+					onChange={handleOptions}
+					checked={options.topSide}
+				/>
+				<Checkbox
+					title="Bottom Side"
+					name="bottomSide"
+					onChange={handleOptions}
+					checked={options.bottomSide}
+				/>
+				<Checkbox
+					title="Left Side"
+					name="leftSide"
+					onChange={handleOptions}
+					checked={options.leftSide}
+				/>
+				<Checkbox
+					title="Right Side"
+					name="rightSide"
+					onChange={handleOptions}
+					checked={options.rightSide}
+				/>
+			</div>
+
+			<Range
+				title="Amount of Points"
+				name="amountOfPoints"
+				onChange={handleOptions}
+				value={options.amountOfPoints}
+			/>
+			<Range
+				title="Roughness"
+				name="roughness"
+				onChange={handleOptions}
+				value={options.roughness}
+			/>
+
+			<div className="flex gap-2 sm:gap-4 mt-2 flex-col sm:flex-row">
+				<button
+					className="btn btn-sm sm:btn-md btn-primary sm:text-xl uppercase sm:grow-[3]"
+					onClick={handleCopyStyles}
+					aria-label="Copy Styles"
+				>
+					Copy Styles
+				</button>
+				<button
+					className="btn btn-sm sm:btn-md btn-primary sm:text-xl uppercase sm:grow-[1]"
+					onClick={handleRefresh}
+					aria-label="Refresh"
+				>
+					Refresh
+				</button>
+			</div>
+		</form>
+	);
+}
