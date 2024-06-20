13436b761db480f54bf9c6da64733d4c9f129d7a
Mike Newberry <hello@mikenew.xyz>
2024-06-20 22:59:49 +0100

    add update paper component

--- src/components/paper/paper.jsx
+++ src/components/paper/paper.jsx
@@ -1,57 +1,20 @@
-import { useRef } from "react";
-import linedPaperPapper from "../../assets/lined-paper-pattern.png";
 import isMobile from "is-mobile";
+import linedPaperPapper from "../../assets/lined-paper-pattern.png";
+import useFloatingCard from "../../hooks/useFloatingCard/useFloatingCard";
 
 export default function Paper({ children }) {
-	const containerRef = useRef();
-	const paperRef = useRef();
-
-	const handleMouseMove = (e) => {
-		if (isMobile()) return;
-
-		requestAnimationFrame(() => {
-			const { top, left } = e.target.getBoundingClientRect();
-			const halfHeight = paperRef.current.offsetHeight / 2;
-			const halfWidth = paperRef.current.offsetWidth / 2;
-			const mouseY = e.clientY - top;
-			const mouseX = e.clientX - left;
-			const rotateY = -((mouseX - halfWidth) / halfWidth);
-			const rotateX = (mouseY - halfHeight) / halfHeight;
-
-			Object.assign(paperRef.current.style, {
-				transform: `
-			    rotateX(${rotateX * 5}deg)
-			    rotateY(${rotateY * 5}deg)
-			    scale(${1.05})
-			  `,
-			});
-		});
-	};
-
-	const handleMouseOut = () => {
-		requestAnimationFrame(() => {
-			Object.assign(paperRef.current.style, {
-				transform: `
-          rotateX(0deg)
-          rotateY(0deg)
-          scale(1)
-        `,
-			});
-		});
-	};
+	const { outerRef, innerRef } = useFloatingCard();
 
 	return (
 		<div
-			ref={containerRef}
+			ref={outerRef}
 			className="w-full sm:w-[30rem] landscape:h-full md:landscape:h-fit overflow-x-visible drop-shadow-md"
-			style={{ perspective: "1000px", overflowY: isMobile() ? "auto" : "visible" }}
+			style={{ overflowY: isMobile() ? "auto" : "visible" }}
 		>
 			<div
-				ref={paperRef}
-				className="w-full bg-base-100 transition-transform duration-500 ease-out select-none"
+				ref={innerRef}
+				className="w-full bg-base-100 transition-transform duration-500 ease-out select-none paper"
 				style={{ background: `url(${linedPaperPapper})` }}
-				onMouseMove={handleMouseMove}
-				onMouseOut={handleMouseOut}
 			>
 				{children}
 			</div>
